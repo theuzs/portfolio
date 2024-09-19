705a517dd4697b6fed7e33e4c1453221c66decd0
theuzs <119632707+theuzs@users.noreply.github.com>
2024-09-19 17:59:52 -0300

    Update Hero.jsx

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import HeroImage from '../assets/hero-image.png';
-
 import './Hero.css'; 
-
 
 const Hero = () => {
   return (
@@ -25,12 +23,17 @@ const Hero = () => {
           including custom Fiori applications and seamless system integrations
         </p>
         <div className='mt-8 space-x-4'>
-            <a href="https://api.whatsapp.com/send?phone=5564992855573&text=Ol%C3%A1%20Matheus,%20tem%20um%20minuto%20?" className='md:flex items-center hidden'>
-            <button
-              className='bg-gradient-to-r from-green-400 to-blue-500 text-white
-              transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
-              Contact With Me
-            </button>
+            <a 
+              href="https://api.whatsapp.com/send?phone=5564992855573&text=Ol%C3%A1%20Matheus,%20tem%20um%20minuto%20?" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              <button
+                className='bg-gradient-to-r from-green-400 to-blue-500 text-white
+                transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
+                Contact With Me
+              </button>
+            </a>
             <button
               className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
               transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
